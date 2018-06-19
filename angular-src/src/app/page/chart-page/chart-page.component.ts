40668939e4c05a8147b952c05ef54ae192ac03a6
mengcheyang <mengche.yang.hh@gmail.com>
2018-06-19 15:35:29 +0800

    update ui

--- angular-src/src/app/page/chart-page/chart-page.component.ts
+++ angular-src/src/app/page/chart-page/chart-page.component.ts
@@ -13,7 +13,7 @@ export class ChartPageComponent implements OnInit {
     {
       'type': 'revenue',
       'name': '營收'
-    },{
+    }, {
       'type': 'profit',
       'name': '毛利'
     }, {
@@ -24,9 +24,9 @@ export class ChartPageComponent implements OnInit {
     {
       'type': 'dayOfMonth',
       'name': '日'
-    },{
+    }, {
       'type': 'month'
-      ,'name': '月'
+      , 'name': '月'
     }, {
       'type': 'year',
       'name': '年'
@@ -41,7 +41,7 @@ export class ChartPageComponent implements OnInit {
   };
 
   // options
-  public barSetting={
+  public barSetting = {
     view: [700, 400],
     showXAxis: true,
     showYAxis: true,
@@ -52,7 +52,7 @@ export class ChartPageComponent implements OnInit {
     showYAxisLabel: true,
     yAxisLabel: 'NT$'
   }
-  public pieSetting={
+  public pieSetting = {
     view: [500, 500],
     legend: false,
     explodeSlices: false,
@@ -60,7 +60,7 @@ export class ChartPageComponent implements OnInit {
     doughnut: false,
     gradient: true,
     isHori: true
-  }  
+  }
 
   public pageSetting = {
     formatType: this.formats[0].type,
@@ -68,19 +68,19 @@ export class ChartPageComponent implements OnInit {
     from: null,
     to: null
   }
-  
+
   colorScheme = {
     domain: [
       '#FAC51D', '#66BD6D', '#FAA026', '#29BB9C', '#E96B56', '#55ACD2', '#B7332F', '#2C83C9', '#9166B8', '#92E7E8'
     ]
-  };  
+  };
   constructor(
     private chartSrv: ChartService,
-  ) {}
+  ) { }
 
   ngOnInit() {
     this.barData = [];
-    this.pieData={
+    this.pieData = {
       'supplier': [],
       'category': []
     }
@@ -89,17 +89,17 @@ export class ChartPageComponent implements OnInit {
     this.queryPieData('supplier');
     this.queryPieData('category');
     let width = window.innerWidth * 1
-    this.setChartSize(width);    
+    this.setChartSize(width);
   }
 
-  private setChartSize(width){
+  private setChartSize(width) {
     this.barSetting.view[0] = width;
-    this.barSetting.view[1] = this.barSetting.view[0]*0.56;
-    if(width > 500){
+    this.barSetting.view[1] = this.barSetting.view[0] * 0.56;
+    if (width > 500) {
       this.pieSetting.view[0] = width / 2;
       this.pieSetting.view[1] = width / 2;
       this.pieSetting.isHori = true;
-    }else{
+    } else {
       this.pieSetting.view[0] = width;
       this.pieSetting.view[1] = width;
       this.pieSetting.isHori = false;
@@ -109,135 +109,153 @@ export class ChartPageComponent implements OnInit {
   onSelect(event) {
     console.log(event);
   }
-    
+
   @HostListener('window:resize', ['$event'])
   onResize(event) {
-    let width = window.innerWidth *1
+    let width = window.innerWidth * 1
     this.setChartSize(width);
   }
 
-  public onFormatChanged(){
+  public onFormatChanged() {
     this.queryHistogram(this.pageSetting.from, this.pageSetting.to, this.pageSetting.group, this.pageSetting.formatType);
   }
 
-  private formatBarData(formatType, res){
+  private formatBarData(formatType, res) {
     let data = [];
-    
-    for(let i in res){
-      let d = [], tmpl='';
-      let item={
-        "name":'',
-        "series":[]
-      };      
-      if(res[i]._id.dayOfWeek){
+
+    for (let i in res) {
+      let d = [], tmpl = '';
+      let item = {
+        "name": '',
+        "series": []
+      };
+      if (res[i]._id.dayOfWeek) {
         item.name = res[i]._id.dayOfWeek;
-      }else{
-        if(res[i]._id.year){
+      } else {
+        if (res[i]._id.year) {
           d.push(res[i]._id.year);
-          tmpl+=('Y');
+          tmpl += ('Y');
         }
-        if(res[i]._id.month){
+        if (res[i]._id.month) {
           d.push(res[i]._id.month);
-          tmpl+=('/M');
+          tmpl += ('/M');
         }
-        if(res[i]._id.date){
+        if (res[i]._id.date) {
           d.push(res[i]._id.date);
-          tmpl+=('/D');
+          tmpl += ('/D');
         }
         item.name = moment(d).format(tmpl);
       }
-      switch(formatType){
+      switch (formatType) {
         case 'revenue':
           item.series.push(
             {
-              "name":"營收",
+              "name": "營收",
               "value": res[i].revenue
             });
-        break;
+          break;
         case 'profit':
           item.series.push(
             {
-              "name":"毛利",
+              "name": "毛利",
               "value": res[i].revenue - res[i].cost
             });
-        break;        
+          break;
         case 'profit-cost':
           item.series.push(
             {
-              "name":"成本",
+              "name": "成本",
               "value": res[i].cost
-            },            
+            },
             {
-              "name":"毛利",
+              "name": "毛利",
               "value": res[i].revenue - res[i].cost
             });
-        break;
+          break;
       }
       data.push(item);
     }
     return data;
   }
-  public weekHistogram(){
+  public weekHistogram() {
     this.pageSetting.from = moment().startOf('week').toDate();
     this.pageSetting.to = moment().endOf('week').toDate();
     this.queryHistogram(this.pageSetting.from, this.pageSetting.to, this.pageSetting.group, this.pageSetting.formatType);
   }
 
-  public monthHistogram(){
+  public monthHistogram() {
     this.pageSetting.from = moment().startOf('month').toDate();
     this.pageSetting.to = moment().endOf('month').toDate();
     this.queryHistogram(this.pageSetting.from, this.pageSetting.to, this.pageSetting.group, this.pageSetting.formatType);
   }
-  
-  public yearHistogram(){
+
+  public yearHistogram() {
     this.pageSetting.from = moment().startOf('year').toDate();
     this.pageSetting.to = moment().endOf('year').toDate();
     this.queryHistogram(this.pageSetting.from, this.pageSetting.to, this.pageSetting.group, this.pageSetting.formatType);
-  }  
+  }
 
-  public onQueryByTimeSlot(){
-    if(moment.isMoment(this.pageSetting.from)){
+  public onQueryByTimeSlot() {
+    if (moment.isMoment(this.pageSetting.from)) {
       this.pageSetting.from = this.pageSetting.from.toDate();
     }
-    if(moment.isMoment(this.pageSetting.to)){
+    if (moment.isMoment(this.pageSetting.to)) {
       this.pageSetting.to = this.pageSetting.to.toDate();
     }
     this.queryHistogram(this.pageSetting.from, this.pageSetting.to, this.pageSetting.group, this.pageSetting.formatType);
   }
 
-  private queryHistogram(from, to, group, formatType){
+  private queryHistogram(from, to, group, formatType) {
     this.queryPieData('supplier');
-    this.queryPieData('category');    
-    this.chartSrv.getHistogramData(from, to, group).subscribe((res)=>{
+    this.queryPieData('category');
+    this.chartSrv.getHistogramData(from, to, group).subscribe((res) => {
       // notify data changed to charts
       this.barData = this.formatBarData(formatType, res);
     });
   }
 
-  public formatPieData(group, res){
-    let data={'revenue': [], 'profit': [], 'quantity': []};
-    for( let i in res){
+  public formatPieData(group, res) {
+    let data = { 'revenue': [], 'profit': [], 'quantity': [] };
+    let totalRevenue = 0, totalCost = 0, totalQuantity = 0;
+    for (let i in res) {
+      totalRevenue += res[i].revenue;
+      totalCost += res[i].cost;
+      totalQuantity += res[i].quantity;
+    }
+    for (let i in res) {
       data.revenue.push({
-        'name': res[i]._id,
-        'value':res[i].revenue
+        'name': res[i]._id + ' (' + Math.round(res[i].revenue / totalRevenue * 10000) / 100 + '%)',
+        'value': res[i].revenue,
+        // 'percentage': Math.round(res[i].revenue / totalRevenue * 10000) / 100
       });
       data.profit.push({
-        'name': res[i]._id,
-        'value':res[i].revenue - res[i].cost
+        'name': res[i]._id + ' (' + Math.round((res[i].revenue - res[i].cost) / (totalRevenue - totalCost) * 10000) / 100 + '%)',
+        'value': res[i].revenue - res[i].cost,
+        // 'percentage': Math.round((res[i].revenue - res[i].cost) / (totalRevenue - totalCost) * 10000) / 100
       });
       data.quantity.push({
-        'name': res[i]._id,
-        'value':res[i].quantity
-      });            
+        'name': res[i]._id + ' (' + Math.round(res[i].quantity / totalQuantity * 10000) / 100 + '%)',
+        'value': res[i].quantity,
+        // 'percentage': Math.round(res[i].quantity / totalQuantity * 10000) / 100
+      });
     }
     return data;
   }
 
 
-  public queryPieData(group){
-    this.chartSrv.getPieData(this.pageSetting.from, this.pageSetting.to, group).subscribe((res)=>{
+  public queryPieData(group) {
+    this.chartSrv.getPieData(this.pageSetting.from, this.pageSetting.to, group).subscribe((res) => {
       this.pieData[group] = this.formatPieData(group, res);
     });
+  }
+
+  public tooltipText(_a) {
+    var data = _a.data;
+    var label = data.name;
+    var val = data.value.toLocaleString();
+    let percentage = Math.round(0.15810881 * (_a.endAngle - _a.startAngle) * 10000) / 100;
+    console.log(JSON.stringify(_a));
+    return "\n      <span class=\"tooltip-label\">" + label + "</span>\n      <span class=\"tooltip-val\">" + val + '(' + percentage + ')' + "</span>\n    ";
   }
 }
 
